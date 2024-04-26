baee3e17264ed642cd2d977b515ba54537e6c645
Phil Travis <physicistphil@gmail.com>
2024-04-25 17:49:42 -0700

    Record every FFC shot if possible (name is often 1 behind -- use timestamps)

--- record-ffc.py
+++ record-ffc.py
@@ -15,9 +15,9 @@ from pyphantom import Phantom, utils, cine
 #   because that would cause many cines to be taken for the specific shot (limitation of 
 #   the datarun sequencer). The camera needs to set the proper settings before the shot
 #   is taken.
-FFC_shot_list = []
-shots_per_position = 10  # please set to the proper value!
-base_save_path = ""  # change to some place with a lot of storage
+FFC_shot_list = []  # shot list for the high-resolution cines
+shots_per_position = 6  # please set to the proper value! This is for low-res cines
+base_save_path = "G:\\saved_MSI\\Datarun_2024-4-14\\cines\\"  # change to some place with a lot of storage, end with a '\\'
 
 
 def record_cine(cam):
@@ -26,7 +26,7 @@ def record_cine(cam):
     cam.record(cine=1, delete_all=True)
     while(cam.partition_recorded(1) is False):
         print("Recording... waiting for cine  ", end='\r')
-        time.sleep(0.1)  # sleep for 100 ms until a cine is recorded
+        time.sleep(0.01)  # sleep for 10 ms until a cine is recorded
     print("Recording... done   \t\t\t\t")
 
 
@@ -34,10 +34,11 @@ def save_cine(cam, frame_range, filename):
     rec_cine = cine.Cine.from_camera(cam, 1)
     rec_cine.save_range = frame_range
     rec_cine.save_non_blocking(filename=filename)
+    # rec_cine.save(filename=filename)
 
     while (rec_cine.save_percentage < 100):
         print("Saving {}%   ".format(rec_cine.save_percentage), end='\r')
-        time.sleep(0.1)
+        time.sleep(0.01)
     print("Saving -- done")
     rec_cine.close()
 
@@ -100,54 +101,123 @@ def record_FFC_footage(q):
 
     # Assumes only one camera on the network
     cam = ph.Camera(0)
+    curr_shot = 0
+
+    msi = pickle.loads(q.get())
+    cine_folder = base_save_path + 'small\\' + msi['datarun_key'] + '\\'
+    print("Recording small cines")
+    # Set camera settings
+    cam.resolution = (256, 256)
+    cam.exposure = 14  # microseconds, I ssume
+    cam.edr_exposure = 0
+    cam.frame_rate = 2500
+    cam.post_trigger_frames = 200  # Save range *must* be less than this or else it will save the entire cine!
+    print("Set small cine parameters -- recording as many shots as possible")
+
+    previous_shotnum=0
 
     while True:
-        msi = pickle.loads(q.get())
+        record_cine(cam)
+
+        while q.empty() is False:  # make sure that we don't get too far behind in shotnumber
+            msi = pickle.loads(q.get())
+        cine_folder = base_save_path + 'small\\' + msi['datarun_key'] + '\\'
         datarun_shotnum = msi['datarun_shotnum']
         ts_sec = datetime.datetime.fromtimestamp(msi['diode_t0_seconds'][0])
         ts_frac = msi['diode_t0_fraction'][0].astype('u8') / 2 ** 64
-        msi_time_string = "{}+{:0.4}".format(ts_sec, ts_frac)
-
-        # If datarun is ongoing and the datarun shotnumber is right before one we want:
-        if msi['datarun_timeout'] is False and (datarun_shotnum + 1) in FFC_shot_list:
-            cine_folder = base_save_path + 'cines/' + msi['datarun_key']
-            filename = 'shot-' + f'{datarun_shotnum:05}' + '_msi-' + msi_time_string
-
-            print("Recording large cine, saving to " + cine_folder + '/' + filename, flush=True)
-            # Set camera settings
-            cam.resolution = (256, 256)
-            cam.exposure = 14  # microseconds, I ssume
-            cam.edr_exposure = 0
-            cam.frame_rate = 35000
-            cam.post_trigger_frames = 2000  # Save range *must* be less than this or else it will save the entire cine!
-            print("Set large cine parameters")
-
-            record_cine(cam)
-            save_cine(cam, utils.FrameRange(-300, 1600), cine_folder + '/' + filename)
-        elif msi['datarun_timeout'] is False and (datarun_shotnum + 1) % shots_per_position == 2:
-            cine_folder = base_save_path + 'cines/small/' + msi['datarun_key']
-            filename = 'shot-' + f'{datarun_shotnum:05}' + '_msi-' + msi_time_string
-
-            print("Recording large cine, saving to " + cine_folder + '/' + filename, flush=True)
-            # Set camera settings
-            cam.resolution = (256, 256)
-            cam.exposure = 14  # microseconds, I ssume
-            cam.edr_exposure = 0
-            cam.frame_rate = 2500
-            cam.post_trigger_frames = 200  # Save range *must* be less than this or else it will save the entire cine!
-            print("Set large cine parameters")
-
-            record_cine(cam)
-            save_cine(cam, utils.FrameRange(-30, 160), cine_folder + '/' + filename)
+        ts_datarun = datetime.datetime.fromtimestamp(int(msi['datarun_timestamp']))
+        ts_datarun_frac = np.modf(msi['datarun_timestamp'])[0]
+        if msi['datarun_timeout'] == 1:
+            print("MSI time: {}+{:0.4}".format(ts_sec, ts_frac), flush=True)
         else:
-            ts_datarun = datetime.datetime.fromtimestamp(int(msi['datarun_timestamp']))
-            ts_datarun_frac = np.modf(msi['datarun_timestamp'])[0]
-            if msi['datarun_timeout'] == 1:
-                print("MSI time: {}+{:0.4}".format(ts_sec, ts_frac), flush=True)
-            else:
-                print("Datarun shot: {}. Time: {}.{:0.4}. "
-                      "MSI time: {}+{:0.4}".format(msi['datarun_shotnum'], ts_datarun,
-                                                   ts_datarun_frac, ts_sec, ts_frac), flush=True)
+            print("Datarun shot: {}. Time: {}.{:0.4}. "
+                  "MSI time: {}+{:0.4}".format(msi['datarun_shotnum'], ts_datarun,
+                                                       ts_datarun_frac, ts_sec, ts_frac), flush=True)
+
+        # if msi['datarun_timeout'] == 0 and (datarun_shotnum % shots_per_position) % 2 == 0:  # save every other shot
+        if msi['datarun_timeout'] == 0:  # save every shot
+            msi_time_string = "{}_{:0.4}".format(ts_sec, ts_frac)
+            msi_time_string = "-".join(msi_time_string.split(':'))
+            msi_time_string = "-".join(msi_time_string.split('.'))
+            filename = 'shot-' + f'{datarun_shotnum:05}' + '_msi-' + msi_time_string
+            filename = cine_folder + filename
+            filename = "-".join(filename.split())
+            print("Saving to " + filename, flush=True)
+            # if previous_shotnum != datarun_shotnum:
+            save_cine(cam, utils.FrameRange(-30, 160), filename)
+            # else:
+                # print("Same shotnumber -- skipping")
+            # previous_shotnum = datarun_shotnum
+        # else:
+        #     time.sleep(1)
+
+
+
+        # if curr_shot == msi['datarun_shotnum']:
+        #     time.sleep(0.1)
+        # else:
+        #     curr_shot = msi['datarun_shotnum']
+
+        #     # If datarun is ongoing and the datarun shotnumber is right before one we want:
+        #     if msi['datarun_timeout'] == 0 and (datarun_shotnum + 1) in FFC_shot_list:
+        #         cine_folder = base_save_path + '' + msi['datarun_key'] + '\\'
+        #         print("Recording large cine")
+        #         # Set camera settings
+        #         cam.resolution = (256, 256)
+        #         cam.exposure = 14  # microseconds, I ssume
+        #         cam.edr_exposure = 0
+        #         cam.frame_rate = 35000
+        #         cam.post_trigger_frames = 2000  # Save range *must* be less than this or else it will save the entire cine!
+        #         print("Set large cine parameters")
+        #         record_cine(cam)
+
+        #         msi = pickle.loads(q.get())
+        #         datarun_shotnum = msi['datarun_shotnum']
+        #         ts_sec = datetime.datetime.fromtimestamp(msi['diode_t0_seconds'][0])
+        #         ts_frac = msi['diode_t0_fraction'][0].astype('u8') / 2 ** 64
+        #         msi_time_string = "{}_{:0.4}".format(ts_sec, ts_frac)
+        #         msi_time_string = "-".join(msi_time_string.split(':'))
+        #         msi_time_string = "-".join(msi_time_string.split('.'))
+        #         filename = 'shot-' + f'{datarun_shotnum:05}' + '_msi-' + msi_time_string
+        #         filename = cine_folder + filename
+        #         filename = "-".join(filename.split())
+        #         print("Saving to " + filename, flush=True)
+        #         save_cine(cam, utils.FrameRange(-300, 1600), filename)
+
+        #     elif msi['datarun_timeout'] == 0 and (datarun_shotnum + 1) % shots_per_position == 2:
+        #         cine_folder = base_save_path + 'small\\' + msi['datarun_key'] + '\\'
+        #         print("Recording small cine")
+        #         # Set camera settings
+        #         cam.resolution = (256, 256)
+        #         cam.exposure = 14  # microseconds, I ssume
+        #         cam.edr_exposure = 0
+        #         cam.frame_rate = 2500
+        #         cam.post_trigger_frames = 200  # Save range *must* be less than this or else it will save the entire cine!
+        #         print("Set small cine parameters -- recording four shots")
+        #         record_cine(cam)
+
+    #             msi = pickle.loads(q.get())
+    #             datarun_shotnum = msi['datarun_shotnum']
+    #             ts_sec = datetime.datetime.fromtimestamp(msi['diode_t0_seconds'][0])
+    #             ts_frac = msi['diode_t0_fraction'][0].astype('u8') / 2 ** 64
+    #             msi_time_string = "{}_{:0.4}".format(ts_sec, ts_frac)
+    #             msi_time_string = "-".join(msi_time_string.split(':'))
+    #             msi_time_string = "-".join(msi_time_string.split('.'))
+    #             filename = 'shot-' + f'{datarun_shotnum:05}' + '_msi-' + msi_time_string
+    #             filename = cine_folder + filename
+    #             filename = "-".join(filename.split())
+    #             print("Saving to " + filename, flush=True)
+    #             save_cine(cam, utils.FrameRange(-30, 160), filename)
+
+        #     else:
+        #         pass
+
+        
+        # cine_folder = base_save_path + 'cines\\small\\' + msi['datarun_key'] + '\\'
+        # filename = 'shot-' + f'{datarun_shotnum:05}' + '_msi-' + msi_time_string
+        # filename = cine_folder + filename
+        # filename = "-".join(filename.split())
+        # print(filename)
 
     cam.close()
     ph.close()
